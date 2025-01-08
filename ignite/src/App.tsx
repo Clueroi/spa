191ddfb70605d71cb5c5027c3857bcbe852da915
unknown <eriicsouzaromeroo@gmail.com>
2025-01-07 22:12:31 -0300

    Add: Styled Components to Project

--- ignite/src/App.tsx
+++ ignite/src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./components/button";
+
 export function App() {
 
   return (
     <>
-      
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
     </>
   )
 }
